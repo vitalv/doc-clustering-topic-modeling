c9a3d13acd58dfc70cc2a20cf4e181d22a5544b2
Vital <uveuve@gmail.com>
2018-02-26 17:30:51 +0100

    added the LSA/SVD part before KMeans, created some new plots, updated presentation

--- doc-clustering-topic-modeling.py
+++ doc-clustering-topic-modeling.py
@@ -9,8 +9,12 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 import scipy.cluster.hierarchy as sch
+import matplotlib.patches as mpatches
 import scipy.spatial.distance as scdist
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics import silhouette_score, silhouette_samples
@@ -20,51 +24,54 @@ sb.set_style("whitegrid", {'axes.grid' : False})
 
 def read_vocab(vocab_file_name):
     '''Reads vocabulary file into a list'''
-	return [w.strip() for w in open(vocab_file_name)]
+    return [w.strip() for w in open(vocab_file_name)]
 
 
 
 
 def read_docword(file_name):
-	'''
-	Reads docword txt file into a Document-Term Matrix (DTM)
-	The full DTM will be too large to hold in memory if represented	as a dense matrix. Use Scipy sparse instead
-	Matrix multiplication involving the sparse representation is rapid thanks to algorithms that avoid explicitly
-	performing multiplications by 0 (nNMF or SVD for instance involve matrix multiplication)
-	'''
+    '''
+    Reads docword txt file into a Document-Term Matrix (DTM)
+    The full DTM will be too large to hold in memory if represented    as a dense matrix. Use Scipy sparse instead
+    Matrix multiplication involving the sparse representation is rapid thanks to algorithms that avoid explicitly
+    performing multiplications by 0 (nNMF or SVD for instance involve matrix multiplication)
+    '''
 
-	file_handle = open(file_name)
-	reader = csv.reader(file_handle, delimiter=' ')
-	D = int(next(reader)[0])
-	W = int(next(reader)[0])
-	N = int(next(reader)[0])
+    file_handle = open(file_name)
+    reader = csv.reader(file_handle, delimiter=' ')
+    D = int(next(reader)[0])
+    W = int(next(reader)[0])
+    N = int(next(reader)[0])
 
-	#create numpy DTM (Document-Term Matrix)
-	m = np.empty(shape=[D,W], dtype='uint8')
-	#instead of creating a sparse matrix and then fill it up, create a numpy matrix
-	#and then later convert it to csr -> SparseEfficiencyWarning
-	#m = sparse.csr_matrix( (D,W), dtype='int8')
+    #create numpy DTM (Document-Term Matrix)
+    m = np.empty(shape=[D,W], dtype='int8')
+    #instead of creating a sparse matrix and then fill it up, create a numpy matrix
+    #and then later convert it to csr -> SparseEfficiencyWarning
+    #m = sparse.csr_matrix( (D,W), dtype='int8')
 
-	for row in reader:
-		D_i = int(row[0])-1
-		W_i = int(row[1])-1
-		count = int(row[2])
-		m[D_i, W_i] = count
+    for row in reader:
+        D_i = int(row[0])-1
+        W_i = int(row[1])-1
+        count = int(row[2])
+        m[D_i, W_i] = count
 
-	m = sparse.csr_matrix(m)
+    m = sparse.csr_matrix(m)
 
-	return D,W,N,m
+    return D,W,N,m
 
 
 #DTM: Document-Term Matrix
-D,W,N,DTM = read_DTM('data/docword.nips.txt')
+docword_file = 'data/docword.kos.txt'
+D,W,N,DTM = read_docword(docword_file)
 
-vocab = read_vocab('data/vocab.nips.txt')
+#Vocabulary
+vocab_file = 'data/vocab.kos.txt'
+vocab = read_vocab(vocab_file)
 
 
 # tfidf, term frequency inverse document frequency
 # is a more reliable metric than plain frequency bc it normalizes frequency across documents
-# very common (and semantically meaningless) words like articles ('the', 'a', 'an' ...), prepositions, etc... are in this way given less weight
+# very common (and semantically meaningless) words like articles ('the', 'and', 'of' ...), prepositions, etc... are in this way given less weight
 
 
 tfidf_transformer = TfidfTransformer()
@@ -83,22 +90,19 @@ A conventional approach involves an -optional- initial step of LSA (Latent Seman
 for dimensionalty reduction followed by K-Means. 
 The downside to this approach in this scenario is that it requires a predefined number of clusters, which is not available
 
-There might not be an optimal number of clusters with complete separation, but there are ways to assess/approximate it.
+There might not be an optimal number of clusters with a good separation, but there are ways to assess/approximate it:
 The 'elbow' method consists of plotting a range of number of clusters on the x axis and the average within-cluster sum of squares
 in the y axis (as a measure of within cluster similarity between its elements). Then an inflexion point would be
 indicative of a good k
 
 Another option to estimate an initial number of clusters consists of running a hierachical clustering and plot a dendrogram
-Depending on the method and metric different results can be achieved. 
+Depending on the method and metric different results can be achieved. But visual inspection can provide a good hint.
 
-If a good candidate for k is found K-Means can be re-run using it as input. In addition, several K-Means runs are advised since
-the algorithm might end up in a local optima. 
-
-
+If a good candidate for k is found, K-Means can be re-run using it as input. 
+In addition, several K-Means runs are advised since the algorithm might end up in a local optima. 
 
 Another approach would be to use a different clustering algorithm not requiring a predefined number of clusters:
-Means-shift, for instance 
-.
+Means-shift, for instance . But this has not provided good results in this scenario (1 or 2 big groups)
 
 '''
 
@@ -106,6 +110,42 @@ Means-shift, for instance
 
 
 
+#SVD/LSA -------------------------------------------------------------------------------------------------------
+'''
+TruncatedSVD implements a variant of singular value decomposition (SVD) that only computes the k largest singular values, 
+where k is a user-specified parameter.
+When truncated SVD is applied to term-document matrices (as returned by CountVectorizer or TfidfVectorizer), 
+this transformation is known as latent semantic analysis (LSA), because it transforms such matrices to a “semantic” space of low dimensionality. 
+In particular, LSA is known to combat the effects of synonymy and polysemy
+(both of which roughly mean there are multiple meanings per word), 
+which cause term-document matrices to be overly sparse and exhibit poor similarity under measures such as cosine similarity.
+TruncatedSVD transformer accepts scipy.sparse matrices without the need to densify them, 
+as densifying may fill up memory even for medium-sized document collections.
+
+The appropriate value of s is a matter of debate, and is application-dependent. Smaller values of s represent a greater extent of
+dimensionality reduction at the cost of a loss of structure of the original DTM. Values anywhere from 30 to 300 are commonly used. 
+In practice, using a cumulative scree plot to select s so that roughly 75–80% of the original variance is recovered tends to
+strike a good balance
+'''
+
+#based on explained variance:
+#kos W/5 -> 83%
+#nips W/10 -> 96%
+n_components = W/5
+
+svd = TruncatedSVD(n_components)
+# DTM_tfidf results are normalized. Since LSA/SVD results are
+# not normalized, we have to redo the normalization:
+normalizer = Normalizer(copy=False)
+lsa = make_pipeline(svd, normalizer)
+
+%time \
+DTM_tfidf_lsa = lsa.fit_transform(DTM_tfidf)
+
+print("Explained variance of the SVD step: {}%".format(int(svd.explained_variance_ratio_.sum() * 100)))
+
+
+#---------------------------------------------------------------------------------------------------------------
 
 
 
@@ -121,32 +161,55 @@ The second step creates new centroids by taking the mean value of all of the sam
 The inertia or within-cluster sum-of-squares is minimized
 '''
 
-k = 20
+k = 8
 km = KMeans(algorithm='auto',
-			copy_x=True,
-			init='k-means++',
-			max_iter=300,
-			n_clusters=k,
-			n_init=10,
-			n_jobs=1,
-			precompute_distances='auto',
-			random_state=None,
-			tol=0.0001,
-			verbose=0)
+            copy_x=True,
+            init='k-means++',
+            max_iter=300,
+            n_clusters=k,
+            n_init=10,
+            n_jobs=1,
+            precompute_distances='auto',
+            random_state=None,
+            tol=0.0001,
+            verbose=0)
 
+
+#sort cluster centers by proximity to centroid and print topics:
+
+# without previous LSA/SVD dimred
+'''
 %time km.fit(DTM_tfidf)
-clusters = km.labels_#.tolist()
-
-#sort cluster centers by proximity to centroid
+clusters = km.labels_
 k_centers = km.cluster_centers_ #Coordinates of cluster centers  [n_clusters, n_features]
 order_centroids = k_centers.argsort()[:, ::-1] #argsort returns the indices that would sort an array
-
 cluster_topics = {}
 for c in range(k):
-	topic = ','.join([vocab[i] for i in [ix for ix in order_centroids[c, :5]]])
-	cluster_topics[c] = topic
-	print "Cluster %i: " % c + topic
+    topic = ','.join([vocab[i] for i in [ix for ix in order_centroids[c, :5]]])
+    cluster_topics[c] = topic
+    print "Cluster %i: " % c + topic
+'''
 
+# with LSA/SVD dimensionality reduction
+%time km.fit(DTM_tfidf_lsa)
+clusters = km.labels_
+
+%time km.fit(DTM_tfidf_lsa)
+clusters = km.labels_
+
+%time km.fit(DTM_tfidf_lsa)
+clusters = km.labels_
+
+# Sort cluster centers by proximity to centroid
+k_centers = km.cluster_centers_ #Coordinates of cluster centers  [n_clusters, n_features]
+original_space_centroids = svd.inverse_transform(k_centers)
+order_centroids = original_space_centroids.argsort()[:, ::-1] #argsort returns the indices that would sort an array
+
+lsa_cluster_topics = {}
+for c in range(k):
+    topic = ','.join([vocab[i] for i in [ix for ix in order_centroids[c, :5]]])
+    lsa_cluster_topics[c] = topic
+    print "Cluster %i: " % c + topic
 
 
 #--------------------------------------------------------------------------------------------------------
@@ -154,23 +217,26 @@ for c in range(k):
 
 
 # Try to find  optimal number of clusters for k-means. "Elbow" method
-k_range = range(2,20)
-kms = [KMeans(n_clusters=k, init='k-means++').fit(DTM_tfidf) for k in k_range]
+#This takes forever without dim red. Use DTM_tfidf_lsa instead of DTM_tfidf
+#Note also that DTM_tfdif_lsa, unlike DTM_tfidf, is not sparse anymore. No need to convert to dense format!
+k_range = range(2,100)
+kms = [KMeans(n_clusters=k, init='k-means++').fit(DTM_tfidf_lsa) for k in k_range]
 centroids = [X.cluster_centers_ for X in kms]
 labels = [km.labels_ for km in kms]
 #calculate Euclidean distance from each point to cluster center
-k_euclid = [scdist.cdist(DTM_tfidf.todense(), c, 'euclidean') for c in centroids]
+k_euclid = [scdist.cdist(DTM_tfidf_lsa, c, 'euclidean') for c in centroids] 
 dist = [np.min(ke, axis=1) for ke in k_euclid]
 #Total within cluster sum of squares
 wcss = [sum(d**2) for d in dist]
 #average wcss
 avwcss = [(sum(d**2))/len(d) for d in dist]
 #total sum of squares
-tss = sum(scdist.pdist(DTM_tfidf.todense())**2)/DTM_tfidf.shape[0]
+tss = sum(scdist.pdist(DTM_tfidf_lsa)**2)/DTM_tfidf_lsa.shape[0]
 #between cluster sum of squares:
 bss = tss - wcss
 #plot average wcss vs number of clusters "Elbow plot": look for a point where the rate of decrease in wcss sharply shifts
 plt.subplots(figsize=(18, 12)) # set size
+plt.title(docword_file + "\n" + "k range: [%s, %s]"%(k_range[0], k_range[-1]))
 plt.plot(k_range, avwcss, '-o')
 plt.ylabel("average wcss")
 plt.xlabel("k")
@@ -185,12 +251,18 @@ from sklearn.metrics import silhouette_score, silhouette_samples
 silhouette_avg_scores = [silhouette_score(DTM_tfidf, l) for l in labels]
 print silhouette_avg_scores
 
+'''
+Silhouette Coefficients range between [-1,1]. 
+Values near +1 indicate that the sample is far away from the neighboring clusters. 
+A value of 0 indicates that the sample is on or very close to the decision boundary between two neighboring clusters 
+and negative values indicate that those samples might have been assigned to the wrong cluster.
+These values are not good, but this measure seems to suffer from the phenomenon called “Concentration of Measure” 
+or “Curse of Dimensionality"
+Check http://scikit-learn.org/stable/auto_examples/text/document_clustering.html
+'''
+
 
 #--------------------------------------------------------------------------------------------------------
-
-
-
-
 
 
 
@@ -215,7 +287,7 @@ from sklearn.cluster import MeanShift, estimate_bandwidth
 #bandwidth dictates the size of the region to search through. (Also called attractive/gravitational interaction length) Can be set manually
 #http://scikit-learn.org/stable/modules/generated/sklearn.cluster.estimate_bandwidth.html
 
-bandwidth = estimate_bandwidth(DTM_tfidf.toarray(), quantile=0.5, n_samples=200) #default: quantile=0.3, n_samples= (all samples are used)
+bandwidth = estimate_bandwidth(DTM_tfidf_lsa)#, quantile=0.5, n_samples=200) #default: quantile=0.3, n_samples= (all samples are used)
 
 #quantile=0.5 means that the median of all pairwise distances is used
 #but it takes a default value if bandwidth is not set
@@ -228,7 +300,7 @@ bandwidth = estimate_bandwidth(DTM_tfidf.toarray(), quantile=0.5, n_samples=200)
 ms = MeanShift(bandwidth=bandwidth)
 #ms = MeanShift()
 
-ms.fit(DTM_tfidf.toarray())
+ms.fit(DTM_tfidf_lsa)
 ms_clusters = ms.labels_
 ms_cluster_centers = ms.cluster_centers_
 
@@ -237,24 +309,11 @@ n_clusters_ = len(ms_cluster_labels_unique)
 
 cluster_idxs = {}
 for cluster in range(n_clusters_):
-	c = labels == cluster
-	cluster_idxs[cluster] = np.where(c == True)
-
-
+    c = labels == cluster
+    cluster_idxs[cluster] = np.where(c == True)
 
 
 #--------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -271,14 +330,14 @@ from sklearn import metrics
 
 #these are all defaults
 db = DBSCAN(eps=0.5, 
-			min_samples=5, 
-			metric='minkowski', 
-			metric_params=None, 
-			algorithm='auto', 
-			leaf_size=30, 
-			p=2)
+            min_samples=5, 
+            metric='minkowski', 
+            algorithm='auto', 
+            leaf_size=30, 
+            p=2)
 
-db.fit(DTM_tfidf)
+#db.fit(DTM_tfidf)
+db.fit(DTM_tfidf_lsa)
 
 
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
@@ -305,56 +364,30 @@ print("Silhouette Coefficient: %0.3f"  % metrics.silhouette_score(DTM_tfidf, lab
 
 
 
-
-
 #Hierarchical Clustering --------------------------------------------------------------------------------
 
-import scipy.cluster.hierarchy as sch
-from sklearn.metrics.pairwise import cosine_similarity
-import scipy.spatial.distance as scdist
-
-
-#Compute distance matrix . Cosine is a good metric
+#Compute distance matrix . Cosine is a good metric. See https://cmry.github.io/notes/euclidean-v-cosine and https://stackoverflow.com/questions/19410270/vector-space-model-cosine-similarity-vs-euclidean-distance
 #Pairwise distances between observations in n-dimensional space.
 
-#Option 1 is sklearn.metrics.pairwise cosine_similarity
-dist = 1 - cosine_similarity(DTM_tfidf)
-
-#Option 2 is scipy.spatial.distance (can't take csr_matrix as input and is slower)
-#D = scdist.pdist(DTM_tfidf.todense(), metric='cosine')
-#Note converting DTM_tfidf to dense format here might be a bad idea, it requires much more memory
-#D = scdist.squareform(D)
-
+#sklearn.metrics.pairwise cosine_similarity
+dist = 1 - cosine_similarity(DTM_tfidf_lsa)
 
 #Then get linkage matrix
-
-#Option 1 define the linkage_matrix using ward clustering pre-computed distances
 linkage_matrix_ward = sch.ward(dist) 
 
-#Option 2 
-linkage_matrix_complete = sch.linkage(D, method='complete')#, metric='cosine')
-
-
-
 #And then plot the dendrogram
-
-dendro_color_threshold = 0.7 #default: 0.7
-
+dendro_color_threshold = 0.12 #default: 0.7
 fig, ax = plt.subplots(figsize=(15, 20)) # set size
-ax = sch.dendrogram(linkage_matrix_complete, orientation="left",color_threshold=dendro_color_threshold*max(linkage_matrix[:,2]));
-
+ax = sch.dendrogram(linkage_matrix_ward, orientation="left",color_threshold=dendro_color_threshold*max(linkage_matrix_ward[:,2]));
 plt.tick_params(\
     axis= 'x',          # changes apply to the x-axis
     which='both',      # both major and minor ticks are affected
     bottom='off',      # ticks along the bottom edge are off
     top='off',         # ticks along the top edge are off
     labelbottom='off')
-
+plt.title(docword_file + "\n" + "Dendrogram color threshold: %s"%dendro_color_threshold)
 plt.tight_layout() #show plot with tight layout
 plt.show()
-
-
-
 
 #--------------------------------------------------------------------------------------------------------
 
@@ -369,92 +402,46 @@ plt.show()
 
 #TSNE --------------------------------------------------------------------------------------------------------
 
-from sklearn.manifold import TSNE
-
 tsne_cos = TSNE(n_components=2, 
-				perplexity=30, 
-				learning_rate=1000, 
-				n_iter=1000, 
-				metric='cosine', 
-				verbose=1)
+                perplexity=30, 
+                learning_rate=1000, 
+                n_iter=1000, 
+                metric='cosine', 
+                verbose=1)
 
-dist = 1 - cosine_similarity(DTM_tfidf)
+#dist = 1 - cosine_similarity(DTM_tfidf)
+dist = 1 - cosine_similarity(DTM_tfidf_lsa)
 
 tsne_cos_coords = tsne_cos.fit_transform(dist)
 
-def scatter(x, colors, nclasses):
-	palette = np.array(sb.color_palette("hls", nclasses )) # color palette with seaborn.
-	f = plt.figure(figsize=(12, 12))
-	ax = plt.subplot(aspect='equal')
-	sc = ax.scatter(x[:,0], x[:,1], linewidth=0, s=40, color=palette[colors])
-	#ax.axis('off')
-	ax.axis('tight')
-	txts = []
-	for i in range(nclasses):
-		# Position of each label.
-		xtext, ytext = np.median(x[colors == i, :], axis=0)
-		txt = ax.text(xtext, ytext, str(i), fontsize=18)
-		txts.append(txt)
-	return f, ax, sc, txts
+def scatter(x, labels, cluster_topics):
+    n_labels = len(set(labels))
+    palette = np.array(sb.color_palette("hls", n_labels )) # color palette with seaborn.
+    f = plt.figure(figsize=(18, 12))
+    ax = plt.subplot(aspect='equal')
+    sc = ax.scatter(x[:,0], x[:,1], linewidth=0, s=40, color=palette[labels])
+    #ax.axis('off')
+    ax.axis('tight')
+    txts = []
+    color_patches = []
+    for i in range(n_labels):
+        # Position of each label
+        patch = mpatches.Patch(color=palette[i], label= "Cluster %i "% i + " " + cluster_topics[i])
+        color_patches.append(patch)
+        xtext, ytext = np.median(x[labels == i, :], axis=0)
+        txt = ax.text(xtext, ytext, str(i), fontsize=18)
+        txts.append(txt)
+    plt.legend(handles=color_patches, bbox_to_anchor=(1.04,1), loc="upper left")
+    plt.subplots_adjust(right=0.60)
+    plt.title(docword_file + "\n" + "k = %s" %len(set(km.labels_)))
+    plt.show()
 
 
 #Plot KMeans clusters 
-
-scatter(tsne_cos_coords, km.labels_, len(set(km.labels_)))
-
+scatter(tsne_cos_coords, km.labels_, lsa_cluster_topics)
 
 
 #--------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
